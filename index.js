4d3edc844d818ff1c3ec8cb1fcd54d33d64e21ab
Derek Mercedes <mercedes.derek@gmail.com>
2023-10-29 16:53:05 -0600

    does taxi math

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(loc){
+
+
+    return Math.abs(42 - loc)
+}
+
+function distanceFromHqInFeet(loc){
+    return Math.abs(42- loc) * 264
+}
+
+function distanceTravelledInFeet(loc1, loc2){
+
+    return Math.abs(loc1 - loc2) *264
+}
+
+function calculatesFarePrice(start, end){
+
+    const distance = distanceTravelledInFeet(start, end)
+
+    if(distance <= 400 ){
+        return 0
+    } else if (distance > 400 && distance <= 2000){
+        return (distance * 2 - 800) / 100
+    } else if (distance > 2000 && distance <= 2500){
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+
+}
